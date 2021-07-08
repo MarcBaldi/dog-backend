b37105cc7c7e2f755c9d8b24c35adac2958029c8
Marcimio <marc.baldi@hotmail.de>
2021-07-08 23:24:35 +0200

    draw hands implemented

--- src/controller/CardController.scala
+++ src/controller/CardController.scala
@@ -1,23 +1,23 @@
 package controller
 
-import model.{Card, Deck, GameRules, Position}
+import model.{Card, Deck, GameRules}
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 class CardController(val gameRules: GameRules) {
   var deck = new Deck()
-  var playerHands: mutable.Map[Int, ArrayBuffer[Card]] = collection.mutable.Map(0 -> new ArrayBuffer[Position]())
+  var playerHands: mutable.Map[Int, ArrayBuffer[Card]] = collection.mutable.Map(0 -> new ArrayBuffer[Card]())
 
   def init(): Unit = {
+    drawHands(6)
+  }
+
+  def drawHands(cardCount: Int): Unit = {
     deck.shuffle()
-
     for (i <- 0 to gameRules.playerCount) {
-
-      playerHands += (i -> deck.draw(6))
-
+      playerHands += (i -> deck.draw(cardCount))
     }
-
   }
 
 }
