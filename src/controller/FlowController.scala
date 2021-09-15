4f0fbfd0ae0cae40c4e16ebf8ea365407f819fd6
Marcimio <marc.baldi@hotmail.de>
2021-09-15 22:23:58 +0200

    controller work

--- src/controller/FlowController.scala
+++ src/controller/FlowController.scala
@@ -1,6 +1,6 @@
 package controller
 
-import model.GameData
+import model.{GameData, GameState}
 
 class FlowController(val gameData: GameData) {
 
@@ -14,15 +14,43 @@ class FlowController(val gameData: GameData) {
   }
 
   def gameLoop(): Unit = {
-
-
     while (true) {
-
-
-
-
+      this.gameTick()
     }
-
   }
 
+  def gameTick(): Unit = {
+    gameData.gameState match  {
+      case GameState.init => this.handleInit()
+      case GameState.preRound => this.handleRound()
+      case GameState.chooseCard => this.handleCard()
+      case GameState.choosePawn => this.handlePawn()
+      case GameState.postTurn => this.handleTurn()
+      case GameState.finished => this.handleFinished()
+      case GameState.end => this.handleEnd()
+    }
+  }
+
+
+  def handleInit(): Unit = {
+
+  }
+  def handleRound(): Unit = {
+
+  }
+  def handleCard(): Unit = {
+
+  }
+  def handlePawn(): Unit = {
+
+  }
+  def handleTurn(): Unit = {
+
+  }
+  def handleFinished(): Unit = {
+
+  }
+  def handleEnd(): Unit = {
+
+  }
 }
--- src/model/GameData.scala
+++ src/model/GameData.scala
@@ -1,5 +1,14 @@
 package model
 
+import scala.collection.mutable
+
 class GameData(val armLength: Int = 15, val playerCount: Int = 4, val pieceAmount: Int = 4)  {
 
+  var currentPlayer: Int = 0
+  var currentRound: Int = 0
+  val finishedPlayers: mutable.ArrayBuffer[Int] = mutable.ArrayBuffer()
+  val enableUndo: Boolean = false
+
+  // state enum
+  var gameState: GameState.Value = GameState.init
 }
